ef623303afe473a84c94e9653a75c32bcba11504
t3rro <t3rro@protonmail.com>
2023-02-23 13:47:40 -0800

    add src

--- flake.nix
+++ flake.nix
@@ -5,11 +5,13 @@
   inputs.ruby-flake-utils.url = github:t3rro/ruby-flake-utils;
 
   outputs = { ruby-flake-utils, nixpkgs, ... }:
-    ruby-flake-utils.lib.mkGemSystems
-      "stitches-terraform-synthesizer"
-      nixpkgs
-      ./Gemfile.lock
-      ./Gemfile
-      ./gemset.nix
-  ;
+    ruby-flake-utils.lib.mkGemSystems {
+      inherit nixpkgs;
+      name = "stitches-terraform-synthesizer";
+      lockfile = ./Gemfile.lock;
+      gemfile = ./Gemfile;
+      gemset = ./gemset.nix;
+      strategy = "lib";
+      src = ./.;
+    };
 }
